bd516a58c1ed4aefb24e3cbdbe1cc379911fb3bc
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2016-06-03 23:11:51 +0800

    Fixes issue #1

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -13,16 +13,7 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $bindings = [
-            App\Repositories\Contracts\OperatorRepositoryInterface::class => App\Repositories\OperatorRepository::class,
-            App\Repositories\Contracts\ScheduleRepositoryInterface::class => App\Repositories\ScheduleRepository::class,
-            App\Repositories\Contracts\StationRepositoryInterface::class  => App\Repositories\StationRepository::class,
-            App\Repositories\Contracts\TrainRepositoryInterface::class    => App\Repositories\TrainRepository::class,
-        ];
-
-        foreach ($bindings as $contract => $repository) {
-            $this->app->bind($contract, $repository);
-        }
+        //
     }
 
     /**
@@ -32,6 +23,16 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $bindings = [
+            \App\Repositories\contracts\RepositoryInterface::class         => \App\Repositories\Repository::class,
+            \App\Repositories\Contracts\OperatorRepositoryInterface::class => \App\Repositories\OperatorRepository::class,
+            \App\Repositories\Contracts\ScheduleRepositoryInterface::class => \App\Repositories\ScheduleRepository::class,
+            \App\Repositories\Contracts\StationRepositoryInterface::class  => \App\Repositories\StationRepository::class,
+            \App\Repositories\Contracts\TrainRepositoryInterface::class    => \App\Repositories\TrainRepository::class,
+        ];
+
+        foreach ($bindings as $contract => $repository) {
+            $this->app->bind($contract, $repository);
+        }
     }
 }
