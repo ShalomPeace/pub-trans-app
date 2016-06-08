8d65fce1c4688667a3f1dc41860c1d31fb54c181
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2016-06-09 00:32:18 +0800

    Query scope for active field and array of timestamps

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -8,8 +8,23 @@ class BaseModel extends Model
 {
     protected $boundToUser = true;
 
+    protected $activeField = 'active';
+
+    public function scopeActive($query, $active = 1) 
+    {
+    	return $query->where($this->activeField, $active);
+    }
+
     public function isBoundToUser()
     {
         return $this->boundToUser;
+    }
+
+    protected function getTImestamps() 
+    {
+        return [
+            'created_at'    => $this->created_at->toDateTimeString(), 
+            'updated_at'    => $this->updated_at->toDateTimeString(),
+        ];
     }
 }
