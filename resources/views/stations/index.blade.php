ecc10ef754644275d3174174c801d69d50c131a0
Shalom Jerusalem <jerusalemshalom23@gmail.com>
2016-06-09 00:55:01 +0800

    Chunk stations to make proper grid layout

--- resources/views/stations/index.blade.php
+++ resources/views/stations/index.blade.php
@@ -6,31 +6,33 @@
             <a href="{!! route('stations.create') !!}" class="btn btn-small btn-info right">Add Station</a>
         </div>
     </section>
-    <section class="row">
-        @if ( ! $stations->isEmpty())
-            @foreach ($stations as $station)
-                <div class="col s4">
-                    <section class="row">
-                        <div class="col s12">
-                           <div class="card">
-                               <div class="card-content">
-                                   <span class="card-title">{{ $station->name }}</span>
-                                   <p>{{ $station->totalSchedules() }} Schedule(s)</p>
-                                   <br>
-                                   <p><strong>Departure:</strong> {{ $station->departureSchedules()->count() }}</p>
-                                   <p><strong>Arrival:</strong> {{ $station->arrivalSchedules()->count() }}</p>
+    @if ( ! $stations->isEmpty())
+        @foreach ($stations->chunk(3) as $chunkStations)
+            <section class="row row-no-mb">
+                @foreach ($chunkStations as $station)
+                    <div class="col s4">
+                        <section class="row row-no-mb">
+                            <div class="col s12">
+                               <div class="card">
+                                   <div class="card-content">
+                                       <span class="card-title">{{ $station->name }}</span>
+                                       <p>{{ $station->totalSchedules() }} Schedule(s)</p>
+                                       <br>
+                                       <p><strong>Departure:</strong> {{ $station->departureSchedules()->count() }}</p>
+                                       <p><strong>Arrival:</strong> {{ $station->arrivalSchedules()->count() }}</p>
+                                   </div>
+                                   <div class="card-action">
+                                       <a href="{!! route('stations.show', $station) !!}">View</a>
+                                       @if (auth()->check())
+                                       <a href="{!! route('stations.edit', $station) !!}">Edit</a>
+                                       @endif
+                                   </div>
                                </div>
-                               <div class="card-action">
-                                   <a href="{!! route('stations.show', $station) !!}">View</a>
-                                   @if (auth()->check())
-                                   <a href="{!! route('stations.edit', $station) !!}">Edit</a>
-                                   @endif
-                               </div>
-                           </div>
-                        </div>
-                    </section>
-                </div>
-            @endforeach
-        @endif
-    </section>
+                            </div>
+                        </section>
+                    </div>
+                @endforeach
+            </section>
+        @endforeach
+    @endif
 @endsection
